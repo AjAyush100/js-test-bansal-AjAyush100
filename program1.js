08498d750dba018b421b7b87b9ee94c863444a69
Ayush Soni <126321124+AjAyush100@users.noreply.github.com>
2024-02-23 13:42:19 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
-};
-
-module.exports = { isValid };
-
-
+    const stack = [];
+    const pairs = {
+      ')': '(',
+      '}': '{',
+      ']': '[',
+    };
+  
+    for (const char of s) {
+      if (pairs[char]) { // If it's a closing bracket
+        const top = stack.pop();
+        if (top !== pairs[char]) {
+          return false; // Doesn't match the top opening bracket
+        }
+      } else { // If it's an opening bracket
+        stack.push(char);
+      }
+    }
+  
+    return stack.length === 0; // Valid only if all opening brackets are closed
+  };
+  
+  module.exports = { isValid };
+  
