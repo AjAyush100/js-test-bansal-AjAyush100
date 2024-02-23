d592e94f6037d755d2f75c2ffc5793f4e02827ba
Ayush Soni <126321124+AjAyush100@users.noreply.github.com>
2024-02-23 13:41:43 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+    let result = 0;
     
-};
-
-
-module.exports={romanToInt}
+    for (let i = 0; i < s.length; i++) {
+      const current = romanMap[s[i]];
+      const next = romanMap[s[i + 1]];
+      
+      if (current < next) {
+        result += next - current;
+        i++; // Skip the next character as it's subtracted
+      } else {
+        result += current;
+      }
+    }
+    
+    return result;
+  };
+  
+  module.exports = { romanToInt };
+  
